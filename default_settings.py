fix default payoffs + long-term payoff calculation

default_settings.py
@@ -3,8 +3,8 @@ import numpy as np
 
 defaults = {
     # Define the standard short-term payoff vectors for a PD
-    'X': np.array([[5, 3, 1, 0]]),
-    'Y': np.array([[5, 1, 3, 0]]),
+    'X': np.array([[3, 0, 5, 1]]),
+    'Y': np.array([[3, 5, 0, 1]]),
 
     # Default probs. for cooperating
     'p': (1, 0, 1, 0),